b76fb3be60c6f1a4003f512e1e6c04556777aa79
veriko lomadze <extvelom@pandora.net>
2022-12-09 12:19:32 +0400

    added new interface

--- src/LoggerClass.java
+++ src/LoggerClass.java
@@ -3,11 +3,10 @@ import org.apache.logging.log4j.Logger;
 
 
 public class LoggerClass {
-<<<<<<< HEAD
+
     private final static Logger LOGGER = LogManager.getLogger(LoggerClass.class);
-=======
-    private final static org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(LoggerClass.class);
->>>>>>> origin/master
+
+
     public static void main(String[] args){
         System.out.println("info");
 
